b70fa8ef3c9249a5b14427d5a2e6a260b622aa52
unax26203 <unaxzardoya@gmail.com>
2025-03-08 13:01:37 +0100

    Add the final translations and make them change instantly.

--- app/src/main/java/com/example/appseguimiento/SettingsActivity.java
+++ app/src/main/java/com/example/appseguimiento/SettingsActivity.java
@@ -1,9 +1,28 @@
 package com.example.appseguimiento;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
+
+import java.util.Locale;
 
 public class SettingsActivity extends AppCompatActivity {
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(newBase);
+        String lang = prefs.getString("language_preference", "es");
+        Locale newLocale = new Locale(lang);
+        Locale.setDefault(newLocale);
+
+        Configuration config = newBase.getResources().getConfiguration();
+        config.setLocale(newLocale);
+        Context context = newBase.createConfigurationContext(config);
+
+        super.attachBaseContext(context);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
