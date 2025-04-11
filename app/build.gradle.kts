b798885a9f412937d556dcf9f60784f7b927c44f
unax26203 <unaxzardoya@gmail.com>
2025-04-11 18:52:28 +0200

    create branch for the second task and upload missing files

---
+++ app/build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.appseguimiento"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.appseguimiento"
+        minSdk = 29
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    implementation(libs.preference)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+
+    // -------------------------------
+    // Dependencias añadidas para Room, RecyclerView y CardView:
+    // Para Room (en Java usamos annotationProcessor en lugar de kapt)
+    implementation(libs.room.runtime)
+    annotationProcessor(libs.room.compiler)
+
+    // Para RecyclerView
+    implementation (libs.recyclerview)
+
+    // Para CardView
+    implementation(libs.cardview)
+}
